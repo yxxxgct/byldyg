2c5dc457e4ccfc1f19cfde5655ab83f2e2d8886b
yxxxgct <1504612398@qq.com>
2025-03-09 19:50:26 +0800

    Add files via upload

---
+++ 单链表.cpp
@@ -0,0 +1,163 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int ElemType;
+
+typedef struct node{
+	ElemType data;
+	struct node *next;
+}Node;
+
+//初化链表
+Node* initList()
+{
+	Node *head=(Node*)malloc(sizeof(Node));
+	head->data=0;
+	head->next=NULL;
+	return head;
+ } 
+ 
+//头插法
+ int insertHead(Node* L,ElemType e)
+ {
+ 	Node *p=(Node*)malloc(sizeof(Node));
+ 	p->data=e;
+ 	p->next=L->next;
+ 	L->next=p;
+ 	return 1;
+  } 
+  
+//遍历
+ void listNode(Node* L)
+{
+	Node *p=L->next;
+	while(p!=NULL)
+	{
+		printf("%d",p->data);
+		p=p->next;
+	}
+	printf("\n");
+ }  
+ 
+ //获取尾部结点
+ //Node* get_tail(Node *L)
+ //{
+ 	//Node *p=L;
+ 	//while(p->next!=NULL)
+ 	//{
+ 		//p=p->next;
+	//}
+	//return p;
+  //} 
+ 
+//尾插法
+//Node*insertTail(Node *tail,ElemType e)
+//{
+	//Node *p=(Node*)malloc(sizeof(Node));
+	//p->data=e;
+	//tail->next=p;
+	//p->next=NULL;
+	//return p;
+// } 
+
+//指定位置插入
+int insertNode(Node *L,int pos,ElemType e)
+{
+	//用来保存插入位置的前驱节点
+	Node *p=L;
+	int i=0;
+	//遍历链表找到插入位置的前驱节点
+	while(i<pos-1)
+	{
+		p=p->next;
+		i++;
+		if(p==NULL)
+		{
+			return 0;
+		}
+	 } 
+	 
+	 //要插入的新节点
+	 Node *q=(Node*)malloc(sizeof(Node));
+	 q->data=e;
+	 q->next=p->next;
+	 p->next=q;
+	 return 1; 
+}
+
+//删除节点
+int deleteNode(Node *L,int pos)
+{
+	//要删除节点的前驱
+	Node *p=L;
+	int i=0;
+	//遍历链表，找到要删除节点的前驱
+	while(i<pos-1)
+	{
+		p=p->next;
+		i++;
+		if(p==NULL)
+		{
+			return 0;
+		}
+	 } 
+	 
+	 if(p->next==NULL)
+	 {
+	 	printf("要删除的位置错误\n");
+	 	return 0;
+	 }
+	 //q指向要删除的节点
+	 Node *q=p->next;
+	 //让要删除节点的前驱指向要删除节点的后继
+	 p->next=q->next;
+	 //释放要删除节点的内存空间
+	 free(q);
+	 return 1; 
+}
+
+//获取链表长度
+int listLength(Node *L)
+{
+	Node *p=L;
+	int len=0;
+	while(p!=NULL)
+	{
+		p=p->next;
+		len++;
+	}
+	return len;
+ } 
+ 
+//释放链表
+void freeList(Node *L)
+{
+ 	Node *p=L->next;
+ 	Node *q;
+ 	
+ 	while(p!=NULL)
+ 	{
+ 		q=p->next;
+ 		free(p);
+ 		p=q;
+	 }
+	 L->next=NULL;
+  } 
+ 
+int main(int argc,char const *argv[]) 
+{
+	Node *list=initList();
+//  Node *tail=get_tail(list);
+	insertHead(list,10);
+	insertHead(list,20);
+	insertHead(list,30);
+	listNode(list);
+	insertNode(list,2,15);
+	listNode(list);
+	deleteNode(list,2);
+	listNode(list);
+	printf("%d\n",listLength(list));
+	freeList(list);
+	printf("%d\n",listLength(list));
+	return 0;
+}
